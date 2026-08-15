377034e615f8f5d1c2a5765022e05a0e02acc917
Jake <git@jklr.org>
2026-08-16 01:23:22 +0200

    Fix MPC float compile errors

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -722,12 +722,12 @@
   // #define MPC_BLOCK_HEAT_CAPACITY { 16.7f }           // (J/K) Heat block heat capacities.
   // #define MPC_SENSOR_RESPONSIVENESS { 0.22f }         // (K/s per ∆K) Rate of change of sensor temperature from heat block.
   // #define MPC_AMBIENT_XFER_COEFF { 0.068f }           // (W/K) Heat transfer coefficients from heat block to room air with fan off.
-  #define MPC_BLOCK_HEAT_CAPACITY 13.92f
-  #define MPC_SENSOR_RESPONSIVENESS 0.0973f
-  #define MPC_AMBIENT_XFER_COEFF 0.0768f
+  #define MPC_BLOCK_HEAT_CAPACITY {13.92f}
+  #define MPC_SENSOR_RESPONSIVENESS {0.0973f}
+  #define MPC_AMBIENT_XFER_COEFF {0.0768f}
   #if ENABLED(MPC_INCLUDE_FAN)
     // #define MPC_AMBIENT_XFER_COEFF_FAN255 { 0.097f }  // (W/K) Heat transfer coefficients from heat block to room air with fan on full.
-    #define MPC_AMBIENT_XFER_COEFF_FAN255 0.0994f
+    #define MPC_AMBIENT_XFER_COEFF_FAN255 {0.0994f}
   #endif
 
   // For one fan and multiple hotends MPC needs to know how to apply the fan cooling effect.
